6f754cbb57cbca6c5b7964912ea4985ac9e14dd0
Denis <ekzemadev@gmail.com>
2020-02-17 12:36:12 +0200

    delete comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,6 @@ class User < ApplicationRecord
                       message: 'Phone numbers must be in xxx-xxx-xxxx format.', if: :phone_present?
   validate :email_or_phone
   validates :password, confirmation: true
-  # validates :phone, presence: true
-  # validates :phone, uniqueness: true
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   include Devise::JWT::RevocationStrategies::JTIMatcher
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
