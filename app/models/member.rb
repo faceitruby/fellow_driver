42ad70819e98f7a536bd5b26ffc06246f3fcc999
magas4891 <magasishe@gmail.com>
2020-02-24 17:14:50 +0200

    primitive validations

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,8 +4,16 @@ class Member < ApplicationRecord
   belongs_to :user
 
   validates :first_name, :last_name, :birth_day, :relationship, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, if: :email_present?
+  validates_format_of :phone,
+                      with: /\(?[0-9]{3}\)?-[0-9]{3}-[0-9]{4}/,
+                      message: 'Phone numbers must be in xxx-xxx-xxxx format.', if: :phone_present?
   validate :email_or_phone
-  # validates :relationship, inclusion: %w(daughter son husband wife)      #may be
+  validates_format_of :birth_day,
+                      with: /(\d{2})\.(\d{2})\.(\d{4})/,
+                      message: 'Birth date must be in xx.xx.xxxx format.'
+  validates :relationship, inclusion: %w(daughter son husband wife father mother)
+
   private
 
   def email_present?
