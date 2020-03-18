c11de0fb1361fb6b17bc28fddb31f9f53c14d0e8
Maksym Bobryshev <maksym.bobmm@gmail.com>
2020-03-18 10:50:59 +0200

    Move file json_web_token.rb from app/lib to lib

--- app/services/users/application_service.rb
+++ app/services/users/application_service.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json_web_token'
+
 # ApplicationService with jwt_encode and jwt_decode methods
 class Users::ApplicationService < ApplicationService
   private
