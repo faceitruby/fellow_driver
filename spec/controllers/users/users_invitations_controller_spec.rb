20447388be74ac74f015aca68a87bf108b1a26da
magas4891 <magasishe@gmail.com>
2020-03-20 16:54:05 +0200

    inv_controller spec done

--- spec/controllers/users/users_invitations_controller_spec.rb
+++ spec/controllers/users/users_invitations_controller_spec.rb
@@ -6,18 +6,6 @@ RSpec.describe Users::InvitationsController, type: :controller do
   let(:user) { create(:user) }
   let(:token) { JsonWebToken.encode(user_id: user.id) }
   let(:params) do
-    # {
-    #   "first_name": "Sergey",
-    #   "last_name": "Magas",
-    #   "phone": "123-456-7890",
-    #   "email": "qwer@as.qw",
-    #   "member_type": Family.member_types.keys.sample,
-    #   "avatar": Rack::Test::UploadedFile.new(ENV['LOCAL_IMAGE_PATH']),
-    #   "address": "asdsd",
-    #   "password": "password",
-    #   "password_confirmation": "password",
-    #   "invitation_token": "213213213"
-    # }
     {
       "first_name": Faker::Name.first_name,
       "last_name": Faker::Name.last_name,
@@ -30,9 +18,15 @@ RSpec.describe Users::InvitationsController, type: :controller do
       "password_confirmation": "password"
     }
   end
-  let(:send_post_request) { post :create, params: params.merge(user: user), as: :json }
-  let(:send_put_request) { put :update, params: params.merge(user: user), as: :json }
-  let(:send_incorrect_post_request) { post :create, params: params.merge(user: user, phone: nil), as: :json }
+  let(:send_post_request) { post :create,
+                            params: params.merge(user: user),
+                            as: :json }
+  let(:send_put_request) { put :update,
+                           params: params.merge(user: user),
+                           as: :json }
+  let(:send_incorrect_post_request) { post :create,
+                                      params: params.merge(user: user,
+                                      phone: nil), as: :json }
 
   describe 'POST#create' do
     before do
@@ -41,57 +35,71 @@ RSpec.describe Users::InvitationsController, type: :controller do
       allow(Twilio::TwilioTextMessenger).to receive(:perform)
     end
 
-    subject { send_post_request }
-
-    xit 'returns json' do
-      subject
+    it 'returns json' do
+      send_post_request
       expect(response.content_type).to include('application/json')
     end
-    it 'has status CREATED' do
-      subject
-      pp response
-      expect(response).to have_http_status(:created)
+
+    context 'when invites' do
+      it 'creates user' do
+        expect { send_post_request }.to change(User, :count).by(1)
+      end
+
+      it 'creates family' do
+        expect { send_post_request }.to change(Family, :count).by(1)
+      end
     end
 
-    # context 'when invites' do
-    #   it 'creates user' do
-    #     expect { subject }.to change(User, :count).by(1)
-    #   end
-    #
-    #   it 'creates family' do
-    #     expect { subject }.to change(Family, :count).by(1)
-    #   end
-    # end
+    context 'with correct request' do
+      before { send_post_request }
+      subject { response }
 
-    # context 'with correct request' do
-    #   before { send_post_request }
-    #   subject { response }
-    #   it_behaves_like 'success action'
-    # end
-    #
-    # context 'with incorrect request' do
-    #   before { send_incorrect_post_request }
-    #   subject { response }
-    #   it_behaves_like 'failure action'
-    # end
+      it { expect(response).to have_http_status(:created) }
+      it_behaves_like 'success action'
+    end
+
+    context 'with incorrect request' do
+      before { send_incorrect_post_request }
+      subject { response }
+      it_behaves_like 'failure action'
+    end
   end
 
-  # describe 'PUT#update' do
-  #   before do
-  #     @request.env['devise.mapping'] = Devise.mappings[:user]
-  #     request.headers['token'] = token
-  #   end
-  #
-  #   subject { send_put_request }
-  #
-  #   it 'returns json' do
-  #     subject
-  #     pp response
-  #     expect(response.content_type).to include('application/json')
-  #   end
-  #   it 'has status ACCEPTED' do
-  #     subject
-  #     expect(response).to have_http_status(:accepted)
-  #   end
-  # end
+  describe 'PUT#update' do
+    before do
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+      request.headers['token'] = token
+      allow(Twilio::TwilioTextMessenger).to receive(:perform)
+    end
+
+    it 'returns json' do
+      send_put_request
+      expect(response.content_type).to include('application/json')
+    end
+
+    context 'with correct request' do
+      before do
+        send_post_request
+        params['invitation_token'] =
+            response.parsed_body['data']['invite_token']
+        send_put_request
+      end
+
+      subject { response }
+
+      it { expect(response).to have_http_status(:accepted) }
+      it_behaves_like 'success action'
+    end
+
+    context 'with incorrect request' do
+      before do
+        send_post_request
+        send_put_request
+      end
+
+      subject { response }
+
+      it_behaves_like 'failure action'
+    end
+  end
 end
