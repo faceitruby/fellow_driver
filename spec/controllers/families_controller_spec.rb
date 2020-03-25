2787bb6754a1b21090e414d21bcfeffbbf718bff
magas4891 <magasishe@gmail.com>
2020-03-25 16:57:30 +0200

    family controller spec

--- spec/controllers/families_controller_spec.rb
+++ spec/controllers/families_controller_spec.rb
@@ -4,6 +4,31 @@ require 'rails_helper'
 
 RSpec.describe FamiliesController, type: :controller do
   describe 'routing' do
-    it { expect(get: '/api/families').to route_to(controller: 'families', action: 'index', format: 'json') }
+    it { expect(get: '/api/families').to route_to(controller: 'families', action: 'index', format: :json) }
+  end
+
+  let(:current_user) { create(:user) }
+  let(:user) { create(:user) }
+  let(:family) { create(:family, owner: current_user.id) }
+  let(:token) { JsonWebToken.encode(user_id: current_user.id) }
+
+  describe 'GET#index' do
+    let(:send_request) { get :index }
+    let(:expected_response) do
+      [family.present.family_page_context].to_json
+    end
+
+    before do
+      request.headers['token'] = token
+      user
+      family
+      send_request
+    end
+
+    it { expect(response.content_type).to include('application/json') }
+    it { expect(response).to have_http_status(:success) }
+    it 'render JSON with list of members' do
+      expect(response.body).to eq(expected_response)
+    end
   end
 end
